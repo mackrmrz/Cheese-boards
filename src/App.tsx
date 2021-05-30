41916308255b016924df1bc5bfbca0436c749804
Mack Ramirez <mack.n.ramirez@gmail.com>
2021-05-30 00:28:39 -0700

    Initial Clean up

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+// Photo by <a href="https://unsplash.com/@callmeed?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erik Dungan</a> on <a href="https://unsplash.com/s/photos/cheese-boards?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+const App = () => {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      
+    </div>
+  );
+}
+
+export default App;
